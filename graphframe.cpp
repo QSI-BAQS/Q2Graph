8c8a96085ff9c08cd5e5fbd16e201c6a52b04d78
Greg Bowen <gregory.bowen@student.uts.edu.au>
2021-09-07 14:32:39 +1000

    TO DO: map d_edges to scene & limit a_vertices elements

--- graphframe.cpp
+++ graphframe.cpp
@@ -460,7 +460,7 @@ void GraphFrame::gf_localComplementation(GraphVertex * lcv) {
             vertexpos.first= vpos;
             vertexpos.second= vprimepos;
 
-            // prohibit autopairs from all_vertex_pos
+            // exclude 'auto-pairs' from all_vertex_pos
             if(vertexpos.first != vertexpos.second)
                all_vertex_pos.push_back(vertexpos);
          }
@@ -471,26 +471,65 @@ void GraphFrame::gf_localComplementation(GraphVertex * lcv) {
       // Vector: any neighbouring vertices for (edge) ADD operation
       QVector<QPair<GraphVertex *, GraphVertex *>> a_vertices {};
 
-      // edge collection: DELETE
+//QVector<QPair<QPointF, QPointF>> look12v;
+
+      // by neighbour vertex, collect any ADD edges or DELETE edges
       for (QPair<QPointF, QPointF> d : all_vertex_pos) {
-         QList<QGraphicsItem *> l1= items(d.first);
-         QList<QGraphicsItem *> l2= items(d.second);
-
-         GraphVertex * v1= qgraphicsitem_cast<GraphVertex *>(l1.first());
-         GraphVertex * v2= qgraphicsitem_cast<GraphVertex *>(l2.first());
-
-         if(items().contains(GraphEdge(v1,v2,edgemenu)))
-      }
-
+         // for each neighbour vertex...
+         for(GraphVertex * p_nvx : qAsConst(neighbourvs)) {
+            // ... identify the edges it shares with other neighbour vertices
+            // and derive any edge to a neighbour vertex that does not exist
+            for (GraphEdge * e : *p_nvx->lcEdges()) {
+             GraphVertex * p1v= e->p1v();
+             GraphVertex * p2v= e->p2v();
 /*
-         // IFF p_nvx has no connecting edge to another neighbour of vertex X,
-         // add a common edge
+QPointF look1= p1v->pos();
+QPointF look2= p2v->pos();
+QPair<QPointF, QPointF> look1d1;
+look1d1.first= look1;
+look1d1.second= d.first;
+look12v.push_back(look1d1);
+QPair<QPointF, QPointF> look2d2;
+look2d2.first= look2;
+look2d2.second= d.second;
+look12v.push_back(look2d2);*/
+             // capture an existing edge between neighbours in d_edges
+             if((d.first == p1v->pos() && d.second == p2v->pos())
+                   && !d_edges.contains(e))
+                   d_edges.push_back(e);
+             // add any 'no match' to a_vertices
+             else if ((d.first != p1v->pos() && d.second != p2v->pos())
+                      && (lcv != p1v && lcv != p2v)){
+                QPair<GraphVertex *, GraphVertex *> adde;
+                adde.first= p1v;
+                adde.second= p2v;
+                if(!a_vertices.contains(adde))
+                   a_vertices.push_back(adde);
+             }
+            }
+         }
+      }
+//qDebug() << "lcv->pos():" << lcv->pos() << " d_edges:" << d_edges.mid(0) << " a_vertices:" << a_vertices.mid(0)/* << "look12v:" << look12v.mid(0)*/;
 
-         // IFF p_nvx has a connecting edge to another neighbour of vertex X,
-         // delete the common edge
-         for (GraphEdge * e : d_edges ) {
-            gf_deleteEdge(e);
-         }*/
-   }
+      // IFF p_nvx has no connecting edge to another neighbour of vertex X,
+      // add a common edge
+      for (QPair<GraphVertex *, GraphVertex *> z : a_vertices) {
+         GraphEdge * e= new GraphEdge(z.first, z.second, edgemenu);
+
+         // add the edge to (QVector) 'edges' of vertices p1 and p2
+         z.first->addEdge(e);
+         z.second->addEdge(e);
+
+         // add a QGraphicsLineItem (GraphEdge) to the GraphFrame
+         // (QGraphicsScene) container
+         addItem(e);
+      }
+/*
+      // IFF p_nvx has a connecting edge to another neighbour of vertex X,
+      // delete the common edge
+      for (GraphEdge * e : d_edges ) {
+         gf_deleteEdge(e);
+      }*/
+      }
 }
 
